608a70b888d2f30f13533097d482d37cab0320dd
chpego <38792705+chpego@users.noreply.github.com>
2021-11-15 20:08:06 +0000

    Update switch.py

--- custom_components/fullykiosk/switch.py
+++ custom_components/fullykiosk/switch.py
@@ -43,6 +43,7 @@ class FullySwitch(CoordinatorEntity, SwitchEntity):
             "manufacturer": self.coordinator.data["deviceManufacturer"],
             "model": self.coordinator.data["deviceModel"],
             "sw_version": self.coordinator.data["appVersionName"],
+            "configuration_url": f"http://{self.coordinator.data["ipaddress"]}:2323",
         }
 
     @property
