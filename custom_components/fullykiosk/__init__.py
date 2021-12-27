879e12742c2d34ba3fc3aa7d5b2fcf51064b1bad
Charles Garwood <cgarwood@gmail.com>
2021-12-27 17:05:52 -0500

    Add number platform

--- custom_components/fullykiosk/__init__.py
+++ custom_components/fullykiosk/__init__.py
@@ -14,7 +14,7 @@ from .coordinator import FullyKioskDataUpdateCoordinator
 
 CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({})}, extra=vol.ALLOW_EXTRA)
 
-PLATFORMS = ["binary_sensor", "light", "media_player", "sensor", "switch"]
+PLATFORMS = ["binary_sensor", "light", "media_player", "number", "sensor", "switch"]
 
 _LOGGER = logging.getLogger(__name__)
 
